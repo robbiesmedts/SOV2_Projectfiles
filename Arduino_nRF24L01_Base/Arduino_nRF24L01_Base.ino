ab2b0160090ef4c02d867627abba2bd95947fd57
robbiesm <robbie.smedts@outlook.com>
2020-07-21 13:18:18 +0200

    initial upload
    made a new repository, added all the old project files to it.
    New repository is for all the different project files. Atmel and Arduino

---
+++ Arduino_nRF24L01_Base/Arduino_nRF24L01_Base.ino
@@ -0,0 +1,24 @@
+/*
+ * Start-up for the Arduino Sensor/Actuator side of the project
+ * 
+ */
+//#define DEBUG
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#ifdef DEBUG
+  #include <printf.h>
+#endif
+
+
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+
+}
