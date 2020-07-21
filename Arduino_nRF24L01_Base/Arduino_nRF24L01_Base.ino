5acc914cf6804b5b62e9e9e175a7e558c0e06c24
robbiesm <robbie.smedts@outlook.com>
2020-07-21 22:56:14 +0200

    extra work on arduino project
    added old project files of the artnet test project.
    Arduino project is not yet in test fase

--- Arduino_nRF24L01_Base/Arduino_nRF24L01_Base.ino
+++ Arduino_nRF24L01_Base/Arduino_nRF24L01_Base.ino
@@ -23,15 +23,19 @@
 #endif
 
 RF24 radio(9, 10); //CE, CSN
-const byte localAddr[6] = "000A1";
+const byte localAddr[6] = "Node1";
 
+struct dataStruct{
+  unsigned byte command;
+  uint16_t dataValue;
+}dataPacket;
 /* command table
    0 = use sensor for own actuator
    1 = send sensor value to other actuator
    2 = receive sensor value for own actuator
    3 = Stop doing the current command
 */
-volatile byte command;
+
 volatile byte nRF_Status;
 bool dataAvailable = 0;
 
@@ -44,48 +48,53 @@ void setup()
   pinMode(sens_pin, INPUT); //if reading a switch with no external pull-up resistor, change it to INPUT_PULLUP
   pinMode(interrupt_pin, INPUT_PULLUP);
   pinMode(act_pin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(interrupt_pin), nRF_ReadData, LOW);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin), nRF_IRQ, LOW);
 
 #ifdef DEBUG
   Serial.begin(115200);
   printf_begin();
 #endif
-
+  /*
+     Initailisation of the nRF24L01 chip
+  */
   radio.begin();
   radio.openReadingPipe(0, localAddr);
   radio.setPALevel(RF24_PA_MIN);
-  //radio.maskIRQ(1,1,0); //mask transmission complete and transmit failure interrupts; interrupt only occurs when a payload is received
   radio.startListening();
 
 #ifdef DEBUG
+  //print all settings of nRF24L01
   radio.printDetails();
 #endif
 }
 
 void loop()
 {
-  if (dataAvailable)
-  {
-    
-  }
+  uint8_t destAddr[40] = "";
+  uint8_t destData;
+
   switch (command)
   {
     case 0:
       do  //read sens_pin and do something on act_pin
       {
-
-      } while (command != 3); //!stop command
+        analogWrite(act_pin, analogRead(sens_pin));
+        delay(10)
+      } while (command = 0); //!stop command
       break;
     case 1:
+      radio.stopListening();
+      radio.openWritingPipe(destAddr)//set destination address
       do //read destination adress, read sens_pin, send data to destination address
       {
 
       } while (command != 3); //!stop command
       break;
     case 2:
+      radio.openReadingPipe(1, destAddr);//set address 2
       do  //receive data and use data for own actuator
       {
-
+        
       } while (command != 3);
       break;
     case 3:
@@ -97,26 +106,12 @@ void loop()
 }
 
 void nRF_IRQ()
-{ 
-  noInterrupts();
-  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-  nRF_Status = SPI.transfer(RF24_NOP);
-  SPI.endTransaction();
-
-  dataAvailable = ((nRF_Status & RX_DR)>>6) //if data available x1xx xxxx, shift right and save
-  
-  interrupts();
-}
-
-void nRF_ReadData()
 {
-
-}
-
-
-
+  noInterrupts();
   if (radio.available())
   {
-    radio.read(&command, sizeof(command));      //read the incomming command
+    radio.read(&command, sizeof(command));
+    dataAvailable = 1;
   }
-  
+  interrupts();
+}
